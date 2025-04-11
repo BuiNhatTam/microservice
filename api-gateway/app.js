6a09014d2534f9862668d9aae9b31b37ea89a6c6
BuiNhatTam <nhattambui88@gmail.com>
2025-04-11 10:32:51 +0700

    add microsevice docker file and docker compose

---
+++ api-gateway/app.js
@@ -0,0 +1,25 @@
+// api-gateway/src/app.js
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Proxy cấu hình cho từng service
+app.use('/products', createProxyMiddleware({
+  target: 'http://product-service:3001',
+  changeOrigin: true
+}));
+
+app.use('/orders', createProxyMiddleware({
+  target: 'http://order-service:3002',
+  changeOrigin: true
+}));
+
+app.use('/customers', createProxyMiddleware({
+  target: 'http://customer-service:3003',
+  changeOrigin: true
+}));
+
+app.listen(3000, () => {
+  console.log('API Gateway running on port 3000');
+});
